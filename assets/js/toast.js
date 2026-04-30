3b137dfa2db9e85cbe185fe0bc539c051b4b7c66
The MadDoG.tmdg <105843577+teremuhamblin@users.noreply.github.com>
2026-04-30 10:35:45 +0200

    Create toast.js

---
+++ assets/js/toast.js
@@ -0,0 +1,8 @@
+export function toast(msg) {
+  const el = document.createElement("div");
+  el.className = "toast";
+  el.textContent = msg;
+  document.body.appendChild(el);
+  setTimeout(() => el.remove(), 3000);
+}
+`
